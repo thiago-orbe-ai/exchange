f6842f56b83b50e415a63e970dd14f3c379a93be
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 00:15:28 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,70 +1,34 @@
 import streamlit as st
+import yfinance as yf
 import pandas as pd
 import numpy as np
-import yfinance as yf
+import matplotlib.pyplot as plt
 
-st.write('## Forex Route of Exchanges')
-st.write('### Data')
-st.write('Exchange rates will be fetched from Yahoo Finance using the yfinance library.')
+# Define the app layout
+st.title("Forex Route Optimizer")
+st.write("Enter the currencies and amount to exchange")
 
-st.write('## Forex Route of Exchanges')
-st.write('### Data')
-st.write('Exchange rates will be fetched from Yahoo Finance using the yfinance library.')
+currency_list = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'HKD', 'NZD', 'SEK', 'KRW', 'SGD', 'NOK', 'MXN', 'INR']
+currency_from = st.selectbox("From", currency_list)
+currency_to = st.selectbox("To", currency_list)
+amount = st.number_input("Amount to exchange", min_value=1)
 
-st.write('### User Inputs')
-from_currency = st.selectbox('Select the currency you want to convert from:', ['USD', 'EUR', 'GBP', 'JPY'])
-to_currency = st.selectbox('Select the currency you want to convert to:', ['USD', 'EUR', 'GBP', 'JPY'])
-amount = st.number_input('Enter the amount you want to convert:', min_value=0.0, max_value=1000000.0, value=1000.0, step=100.0)
+# Get forex data
+forex_data = yf.download(f"{currency_from}{currency_to}=X")["Close"]
 
-st.write('### User Inputs')
-from_currency = st.selectbox('Select the currency you want to convert from:', ['USD', 'EUR', 'GBP', 'JPY'])
-to_currency = st.selectbox('Select the currency you want to convert to:', ['USD', 'EUR', 'GBP', 'JPY'])
-amount = st.number_input('Enter the amount you want to convert:', min_value=0.0, max_value=1000000.0, value=1000.0, step=100.0)
+# Calculate the best forex route
+routes = [['USD', 'EUR', 'JPY', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'JPY', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'JPY', 'EUR', 'USD']]
+best_route = None
+best_rate = None
+for route in routes:
+    rate = 1
+    for i in range(len(route)-1):
+        rate *= forex_data[f"{route[i]}{route[i+1]}=X"][0]
+    if not best_rate or rate > best_rate:
+        best_rate = rate
+        best_route = route
 
-# Define function to get exchange rate from yfinance
-def get_exchange_rate(from_currency, to_currency):
-    ticker = f'{from_currency}{to_currency}=X'
-    exchange_rate = yf.Ticker(ticker).history(period='1d')['Close'][0]
-    return exchange_rate
-
-# Get the available exchange rates for the selected currencies
-available_rates = []
-for currency in [from_currency, to_currency]:
-    if currency != 'USD':
-        usd_rate = get_exchange_rate(currency, 'USD')
-        available_rates.append(usd_rate)
-if len(available_rates) == 1:
-    from_currency = 'USD' if from_currency != 'USD' else to_currency
-    to_currency = 'USD' if to_currency != 'USD' else from_currency
-    usd_rate = get_exchange_rate(from_currency, to_currency)
-    available_rates = [usd_rate]
-else:
-    for currency in ['USD', from_currency, to_currency]:
-        if currency not in [from_currency, to_currency]:
-            usd_rate = get_exchange_rate(currency, 'USD')
-            available_rates.append(usd_rate)
-
-# Calculate the amount of money after each exchange
-amounts_after_exchange = [amount]
-for rate in available_rates:
-    amount_after_exchange = amounts_after_exchange[-1] * rate
-    amounts_after_exchange.append(amount_after_exchange)
-
-# Calculate the percentage change in amount after each exchange
-percent_changes = []
-for i in range(len(amounts_after_exchange)-1):
-    percent_change = (amounts_after_exchange[i+1] - amounts_after_exchange[i]) / amounts_after_exchange[i] * 100
-    percent_changes.append(percent_change)
-
-# Find the index of the maximum percentage change
-max_index = np.argmax(percent_changes)
-
-# Print the optimal forex route
-st.write('### Output')
-st.write('Optimal Forex Route:')
-for i in range(len(available_rates)):
-    rate = available_rates[i]
-    if i == max_index:
-        st.write(f'{from_currency} -> {to_currency} ({rate:.4f}) (Optimal)')
-    else:
-        st.write(f'{from_currency} -> USD ({rate:.4f}) -> {to
+# Display the results
+st.write(f"The best route to exchange {currency_from} to {currency_to} is {' -> '.join(best_route)}")
+st.write(f"The exchange rate is {round(best_rate, 4)}")
+st.write(f"The amount received is {round(amount*best_rate, 2)} {currency_to}")
