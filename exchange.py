27b1f8bab4822de92cec1e7bcd8ef5a6ef6cee1e
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 02:05:36 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,79 +1,61 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 
-def bellman_ford(adj_matrix, source):
-    n = len(adj_matrix)
-    distances = [np.inf for i in range(n)]
-    distances[source] = 0
-    best_routes = [None for i in range(n)]
-
-    for k in range(n-1):
-        for i in range(n):
-            for j in range(n):
-                if adj_matrix[i][j] != 0:
-                    if distances[i] + adj_matrix[i][j] < distances[j]:
-                        distances[j] = distances[i] + adj_matrix[i][j]
-                        best_routes[j] = i
-
-    return distances, best_routes
-
-# Define the distances between major currency pairs
-distances = [
-    [0, 0.8, 1.3, 1.0, 1.2, 1.5, 1.9],
-    [0.8, 0, 1.1, 0.9, 0.9, 1.2, 1.6],
-    [1.3, 1.1, 0, 1.7, 1.6, 1.8, 2.1],
-    [1.0, 0.9, 1.7, 0, 0.8, 1.1, 1.5],
-    [1.2, 0.9, 1.6, 0.8, 0, 1.3, 1.7],
-    [1.5, 1.2, 1.8, 1.1, 1.3, 0, 0.4],
-    [1.9, 1.6, 2.1, 1.5, 1.7, 0.4, 0]
-]
-
-# Define the major currency pairs
+# Define the currency pairs
 currency_pairs = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'USD/CAD', 'AUD/USD', 'NZD/USD']
 
-# Define a dictionary to map currency pairs to their indices in the distance matrix
-currency_index = {currency_pairs[i]: i for i in range(len(currency_pairs))}
-
-# Create a dropdown to select the source currency pair
-source_currency = st.selectbox("Select the source currency pair", currency_pairs)
-
-# Get the index of the source currency pair
-source_index = currency_index[source_currency]
-
-# Calculate the shortest distances and best routes
-distances, best_routes = bellman_ford(distances, source_index)
-
-# Display the results
-st.write("Distance from", source_currency)
-for i in range(len(currency_pairs)):
-    if i != source_index:
-        st.write(source_currency, "to", currency_pairs[i], "=", distances[i])
-
-# Highlight the best route in the chart
-best_routes = [best_routes[i] for i in range(len(best_routes))]
-
-# Create a dictionary to map currency pairs to their colors in the chart
-color_map = {
-    'EUR/USD': 'blue',
-    'USD/JPY': 'orange',
-    'GBP/USD': 'green',
-    'USD/CHF': 'red',
-    'USD/CAD': 'purple',
-    'AUD/USD': 'brown',
-    'NZD/USD': 'pink'
+# Define the currency index
+currency_index = {
+    'EUR/USD': 0,
+    'USD/JPY': 1,
+    'GBP/USD': 2,
+    'USD/CHF': 3,
+    'USD/CAD': 4,
+    'AUD/USD': 5,
+    'NZD/USD': 6
 }
 
+# Define the currency exchange rates
+exchange_rates = np.array([
+    [1.0, 1.2, 0.9, 1.1, 1.3, 0.8, 0.7],
+    [0.8, 1.0, 0.7, 0.9, 1.1, 0.6, 0.5],
+    [1.1, 1.4, 1.0, 1.2, 1.5, 0.9, 0.8],
+    [0.9, 1.1, 0.8, 1.0, 1.2, 0.7, 0.6],
+    [0.7, 0.9, 0.6, 0.8, 1.0, 0.5, 0.4],
+    [1.2, 1.5, 1.1, 1.3, 1.6, 1.0, 0.9],
+    [1.3, 1.6, 1.2, 1.4, 1.7, 1.1, 1.0]
+])
+
+# Define the function to compute the shortest distances and best routes using the Bellman-Ford algorithm
+def bellman_ford(distances, source):
+    n = len(distances)
+    best_routes = [None] * n
+    best_distances = [float('inf')] * n
+    best_distances[source] = 0
+    for _ in range(n - 1):
+        for u in range(n):
+            for v in range(n):
+                if distances[u][v] + best_distances[u] < best_distances[v]:
+                    best_distances[v] = distances[u][v] + best_distances[u]
+                    best_routes[v] = u
+    return best_distances, best_routes
+
+# Compute the distances between currency pairs
+distances = np.zeros((len(currency_pairs), len(currency_pairs)))
+for i in range(len(currency_pairs)):
+    for j in range(len(currency_pairs)):
+        distances[i][j] = -np.log(exchange_rates[i][j])
+
+# Convert the distances matrix to a Pandas DataFrame
+chart_data = pd.DataFrame(data=distances, columns=currency_pairs, index=currency_pairs)
+
 # Add a new column to the chart_data DataFrame with the cumulative distances of the best route
+best_distances, best_routes = bellman_ford(distances, 0)
 chart_data['cumulative_distance'] = np.zeros(len(currency_pairs))
-for i in range(len(best_routes)):
+for i in range(len(currency_pairs)):
     if best_routes[i] is not None:
-        chart_data.loc[i, 'cumulative_distance'] = distances[best_routes[i]]
+        chart_data.loc[currency_pairs[i], 'cumulative_distance'] = -best_distances[i]
 
 # Display the chart
-st.line_chart(chart_data['cumulative_distance'], use_container_width=True)
-
-# Trace the best route
-st.write("Best route:")
-for i in range(len(best_routes)):
-    if best_routes[i] is not None:
-        st.write(currency_pairs[i], "->", currency_pairs[best_routes[i]], "(Distance =", distances[best_routes[i]], ")")
+st
