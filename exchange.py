6e10f9cc01488b4e76d04acd1d97936f5637fe4d
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 02:11:14 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -2,78 +2,74 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Define the currency pairs
-currency_pairs = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'USD/CAD', 'AUD/USD', 'NZD/USD']
+# Define the Bellman-Ford algorithm function
+def bellman_ford(source, currency_pairs, exchange_rates):
+    num_currencies = len(currency_pairs)
+    distances = [np.inf] * num_currencies
+    best_routes = [None] * num_currencies
+    distances[source] = 0
+    
+    for i in range(num_currencies - 1):
+        for j in range(num_currencies):
+            for k in range(num_currencies):
+                if exchange_rates[j][k] != np.inf and distances[j] + np.log(exchange_rates[j][k]) < distances[k]:
+                    distances[k] = distances[j] + np.log(exchange_rates[j][k])
+                    best_routes[k] = j
+                    
+    # Check for negative cycles
+    for j in range(num_currencies):
+        for k in range(num_currencies):
+            if exchange_rates[j][k] != np.inf and distances[j] + np.log(exchange_rates[j][k]) < distances[k]:
+                return None, None, None
+    
+    # Convert distances to negative log-likelihoods
+    for i in range(num_currencies):
+        distances[i] = -distances[i]
+    
+    return distances, best_routes, currency_pairs
 
-# Define the currency index
-currency_index = {
-    'EUR/USD': 0,
-    'USD/JPY': 1,
-    'GBP/USD': 2,
-    'USD/CHF': 3,
-    'USD/CAD': 4,
-    'AUD/USD': 5,
-    'NZD/USD': 6
-}
-
-# Define the currency exchange rates
+# Define the currency pairs and exchange rates
+currency_pairs = ["USD/EUR", "EUR/JPY", "JPY/USD", "USD/GBP", "GBP/EUR", "EUR/CHF", "CHF/JPY"]
 exchange_rates = np.array([
-    [1.0, 1.2, 0.9, 1.1, 1.3, 0.8, 0.7],
-    [0.8, 1.0, 0.7, 0.9, 1.1, 0.6, 0.5],
-    [1.1, 1.4, 1.0, 1.2, 1.5, 0.9, 0.8],
-    [0.9, 1.1, 0.8, 1.0, 1.2, 0.7, 0.6],
-    [0.7, 0.9, 0.6, 0.8, 1.0, 0.5, 0.4],
-    [1.2, 1.5, 1.1, 1.3, 1.6, 1.0, 0.9],
-    [1.3, 1.6, 1.2, 1.4, 1.7, 1.1, 1.0]
+    [np.inf, 0.84, np.inf, 0.72, np.inf, np.inf, np.inf],
+    [1.19, np.inf, 109.91, np.inf, 0.85, np.inf, 130.45],
+    [np.inf, 0.0091, np.inf, np.inf, np.inf, np.inf, 0.0076],
+    [1.38, np.inf, np.inf, np.inf, 0.71, np.inf, np.inf],
+    [np.inf, 1.18, np.inf, 1.41, np.inf, 1.13, np.inf],
+    [1.09, np.inf, np.inf, np.inf, 0.88, np.inf, 117.74],
+    [np.inf, 0.0077, 130.83, np.inf, np.inf, 0.0085, np.inf]
 ])
 
-# Define the function to compute the shortest distances and best routes using the Bellman-Ford algorithm
-def bellman_ford(distances, source):
-    n = len(distances)
-    best_routes = [None] * n
-    best_distances = [float('inf')] * n
-    best_distances[source] = 0
-    for _ in range(n - 1):
-        for u in range(n):
-            for v in range(n):
-                if distances[u][v] + best_distances[u] < best_distances[v]:
-                    best_distances[v] = distances[u][v] + best_distances[u]
-                    best_routes[v] = u
-    return best_distances, best_routes
+# Create the user interface
+st.title("Forex Arbitrage Finder")
 
-# Compute the distances between currency pairs
-distances = np.zeros((len(currency_pairs), len(currency_pairs)))
-for i in range(len(currency_pairs)):
-    for j in range(len(currency_pairs)):
-        distances[i][j] = -np.log(exchange_rates[i][j])
+# Display the currency pairs and exchange rates
+st.write("Currency pairs:")
+st.write(pd.DataFrame(exchange_rates, index=currency_pairs, columns=currency_pairs))
 
-# Convert the distances matrix to a Pandas DataFrame
-chart_data = pd.DataFrame(data=distances, columns=currency_pairs, index=currency_pairs)
+# Select the source currency
+source_currency = st.selectbox("Select the source currency:", currency_pairs)
 
-# Add a new column to the chart_data DataFrame with the cumulative distances of the best route
-best_distances, best_routes = bellman_ford(distances, 0)
-chart_data['cumulative_distance'] = np.zeros(len(currency_pairs))
-for i in range(len(currency_pairs)):
-    if best_routes[i] is not None:
-        chart_data.loc[currency_pairs[i], 'cumulative_distance'] = -best_distances[i]
+# Run the Bellman-Ford algorithm
+distances, best_routes, currency_pairs = bellman_ford(currency_pairs.index(source_currency), currency_pairs, exchange_rates)
 
-# Display the chart
-#Display the chart
-st.line_chart(chart_data['cumulative_distance'], use_container_width=True)
-
-#Trace the best route
-st.write("Best route:")
-for i in range(len(best_routes)):
-    if best_routes[i] is not None:
-        st.write(currency_pairs[i], "->", currency_pairs[best_routes[i]], "(Distance =", -best_distances[i], ")")
-
-#Trace the complete route for the best scenario
-if best_routes[-1] is not None:
-    complete_route = [currency_pairs[-1]]
-    current_currency = currency_pairs[-1]
-    while current_currency != currency_pairs[0]:
-        current_currency = currency_pairs[best_routes[currency_index[current_currency]]]
-        complete_route.insert(0, current_currency)
-        st.write("Complete route:", "->".join(complete_route))
+# Display the results
+if distances is None:
+    st.write("Negative cycle detected")
 else:
-    st.write("No route found")
+    # Create a dataframe to display the distances and best routes
+    data = {"Distance": distances, "Best Route": best_routes}
+    df = pd.DataFrame(data, index=currency_pairs)
+    st.write("Distances and Best Routes:")
+    st.write(df)
+    
+    # Create a chart to show the best route
+    chart_data = pd.DataFrame({"distance": distances, "currency_pair": currency_pairs})
+    chart_data = chart_data.sort_values(by="distance", ascending=False)
+    st.line_chart(chart_data, use_container_width=True)
+
+    # Trace the best route
+    st.write("Best route:")
+    for i in range(len(best_routes)):
+        if best_routes[i] is not None:
+            st.write(currency_pairs[i], "->", currency_pairs[best_routes[i]], "(Distance =", distances[best_routes[i]], ")")
