5e458999dfc577a1e9770bb83aa1b28e1afff60a
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 01:47:30 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -4,10 +4,13 @@ import streamlit as st
 
 # Define the forex graph
 forex_graph = np.array([
-    [0.0, 1.25, 0.9, 0.8],
-    [0.8, 0.0, 0.6, 0.7],
-    [1.1, 1.67, 0.0, 1.2],
-    [1.25, 1.43, 0.83, 0.0]
+    [0.0, 1.17, 1.38, 0.92, 0.78, 110.68, 0.92],
+    [0.85, 0.0, 1.18, 0.79, 0.67, 95.50, 0.85],
+    [0.72, 0.85, 0.0, 0.67, 0.57, 80.12, 0.67],
+    [1.09, 1.29, 1.49, 0.0, 1.18, 132.17, 1.11],
+    [1.28, 1.51, 1.75, 0.84, 0.0, 150.51, 0.84],
+    [0.009, 0.010, 0.012, 0.005, 0.007, 0.0, 0.005],
+    [1.09, 1.29, 1.49, 0.84, 1.18, 132.17, 0.0]
 ])
 
 # Define the Bellman-Ford function
@@ -27,19 +30,20 @@ def bellman_ford(forex_graph, source):
     return distances
 
 # Define the Streamlit app
-st.title('Forex Bellman-Ford')
+def app():
+    st.title('Forex Bellman-Ford')
+    
+    # Get the source currency pair from the user
+    source = st.selectbox('Select the source currency pair:', ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'USD/CAD', 'AUD/USD', 'NZD/USD'])
 
-# Get the source currency pair from the user
-source = st.selectbox('Select the source currency pair:', ['USD/EUR', 'USD/GBP', 'USD/JPY', 'EUR/GBP'])
+    # Map the currency pair to an index in the forex graph
+    source_map = {'EUR/USD': 0, 'USD/JPY': 1, 'GBP/USD': 2, 'USD/CHF': 3, 'USD/CAD': 4, 'AUD/USD': 5, 'NZD/USD': 6}
+    source_index = source_map[source]
 
-# Map the currency pair to an index in the forex graph
-source_map = {'USD/EUR': 0, 'USD/GBP': 1, 'USD/JPY': 2, 'EUR/GBP': 3}
-source_index = source_map[source]
+    # Compute the shortest distances using Bellman-Ford
+    distances = bellman_ford(forex_graph, source_index)
 
-# Compute the shortest distances using Bellman-Ford
-distances = bellman_ford(forex_graph, source_index)
-
-# Display the shortest distances
-for i, distance in enumerate(distances):
-    if i != source_index:
-        st.write(f"{source} to {'USD/EUR' if i == 0 else 'USD/GBP' if i == 1 else 'USD/JPY' if i == 2 else 'EUR/GBP'}: {distance:.2f}")
+    # Display the shortest distances
+    for i, distance in enumerate(distances):
+        if i != source_index:
+            st.write(f"{source} to {'EUR/USD' if i == 0 else 'USD/JPY' if i == 1 else 'GBP/USD' if i == 2 else 'USD/CHF' if i == 3 else 'USD/CAD' if i == 4 else 'AUD/USD' if
