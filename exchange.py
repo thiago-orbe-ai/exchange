50273324ecb9663cff753353a431b81a3ecdedba
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 00:18:19 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -20,15 +20,16 @@ forex_data = yf.download(f"{currency_from}{currency_to}=X")["Close"]
 routes = [['USD', 'EUR', 'JPY', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'JPY', 'USD'], ['USD', 'EUR', 'JPY', 'EUR', 'JPY', 'EUR', 'USD']]
 best_route = None
 best_rate = None
-for route in routes:
-    rate = 1
-    for i in range(len(route)-1):
-        rate *= forex_data[f"{route[i]}{route[i+1]}=X"][0]
-    if not best_rate or rate > best_rate:
-        best_rate = rate
-        best_route = route
+if st.button("Exchange Route"):
+    for route in routes:
+        rate = 1
+        for i in range(len(route)-1):
+            rate *= forex_data[f"{route[i]}{route[i+1]}=X"][0]
+        if not best_rate or rate > best_rate:
+            best_rate = rate
+            best_route = route
 
-# Display the results
-st.write(f"The best route to exchange {currency_from} to {currency_to} is {' -> '.join(best_route)}")
-st.write(f"The exchange rate is {round(best_rate, 4)}")
-st.write(f"The amount received is {round(amount*best_rate, 2)} {currency_to}")
+    # Display the results
+    st.write(f"The best route to exchange {currency_from} to {currency_to} is {' -> '.join(best_route)}")
+    st.write(f"The exchange rate is {round(best_rate, 4)}")
+    st.write(f"The amount received is {round(amount*best_rate, 2)} {currency_to}")
