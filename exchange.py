803785e78acbf8d0571f1ff643f17cb163287527
thiago-orbe-ai <89100381+thiago-orbe-ai@users.noreply.github.com>
2023-04-08 01:08:51 -0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,82 +1,30 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
 import yfinance as yf
 
-def get_exchange_rates(currencies):
-    # Create an empty DataFrame with currencies as columns and index
-    df = pd.DataFrame(columns=currencies, index=currencies)
-    
-    # Fill the DataFrame with exchange rates
-    for i in range(len(currencies)):
-        for j in range(len(currencies)):
-            if i == j:
-                df.iloc[i,j] = 1.0
-            else:
-                pair = currencies[i] + currencies[j] + "=X"
-                data = yf.download(pair, start="2021-01-01", end="2023-04-08")
-                df.iloc[i,j] = data["Close"][-1]
-                
-    return df
+def get_exchange_rate(from_currency, to_currency):
+    ticker = f"{from_currency}{to_currency}=X"
+    data = yf.download(tickers=ticker, period="1d", interval="1m")
+    return data["Close"].iloc[-1]
 
-def get_best_route(df, start_currency):
-    # Create a graph from the DataFrame
-    G = nx.from_pandas_adjacency(df)
-    
-    # Compute the shortest paths from the starting currency
-    paths = nx.shortest_path(G, source=start_currency)
-    
-    # Compute the exchange rate for each path
-    exchange_rates = []
-    for currency, path in paths.items():
-        exchange_rate = 1.0
-        for i in range(len(path)-1):
-            exchange_rate *= df.loc[path[i], path[i+1]]
-        exchange_rates.append((currency, exchange_rate))
-    
-    # Sort the exchange rates and return the best route
-    best_route = sorted(exchange_rates, key=lambda x: x[1])[0]
-    
-    return best_route
+def bellman_ford(currencies):
+    n = len(currencies)
+    distances = [float("inf")] * n
+    distances[0] = 0
+    for i in range(n - 1):
+        for j in range(n):
+            for k in range(n):
+                if j != k:
+                    exchange_rate = get_exchange_rate(currencies[j], currencies[k])
+                    if distances[k] > distances[j] + exchange_rate:
+                        distances[k] = distances[j] + exchange_rate
+    return distances
 
-# Set the page title
-st.set_page_config(page_title="Exchange Route", page_icon=":money_with_wings:")
-
-# Define the app
-# Set the app title
-st.title("Exchange Route")
-
-# Define the available currencies
-currencies = ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY"]
-
-# Get the exchange rates
-exchange_rates = get_exchange_rates(currencies)
-
-# Select the starting currency
-start_currency = st.selectbox
-
-# Select the starting currency
-start_currency = st.selectbox("Select starting currency", currencies)
-# Compute the best route
-if st.button("Run"):
-    best_route = get_best_route(exchange_rates, start_currency)
-    # Display the best route
-    st.write(f"The best exchange route starting from {start_currency} is:")
-    st.write(best_route[0])
-    st.write(f"Exchange rate: {best_route[1]:.4f}")
-
-    # Plot the exchange rates as a heatmap
-    fig, ax = plt.subplots()
-    im = ax.imshow(exchange_rates.values.astype(float), cmap="coolwarm")
-    ax.set_xticks(np.arange(len(currencies)))
-    ax.set_yticks(np.arange(len(currencies)))
-    ax.set_xticklabels(currencies)
-    ax.set_yticklabels(currencies)
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    ax.set_title("Exchange Rates")
-    cbar = ax.figure.colorbar(im, ax=ax)
-    cbar.ax.set_ylabel("Exchange Rate", rotation=-90, va="bottom")
-    st.pyplot(fig)
-
+currencies = st.text_input("Enter currencies separated by commas (e.g. USD,EUR,GBP)").upper().split(",")
+if len(currencies) > 1:
+    distances = bellman_ford(currencies)
+    st.write(f"The best route is {currencies[0]} -> {currencies[-1]}")
+    st.write(f"The total exchange rate is {distances[-1]}")
+else:
+    st.write("Please enter at least two currencies.")
